c8f5a8177cf0bc3fdb6e8e733a3a191d4b46b5de
Nouf <noufalsubaie@Noufs-MacBook-Pro-2.local>
2020-09-07 17:22:32 +0300

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello this is Nouf</Text>
+      <Text>Hello this is Nouf alsubaie</Text>
       <StatusBar style="auto" />
     </View>
   );
