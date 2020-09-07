af9341cf8cebb8f9577acd7febb6cc250341653f
AlanoudAlrashed <61060127+AlanoudAlrashed@users.noreply.github.com>
2020-09-07 17:38:32 +0300

    t

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO WORLD!</Text>
+      <Text>HELLO WORLD!h</Text>
       <StatusBar style="auto" />
     </View>
   );
