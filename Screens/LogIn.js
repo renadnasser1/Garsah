93ba9ce33394d663ae02e905134362b88d984c29
Amal Aldoss <amalabdullah13@gmail.com>
2020-09-17 11:09:43 +0300

    log in logo

--- Screens/LogIn.js
+++ Screens/LogIn.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 
-=======
-import React,{ useState } from "react";
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
 import {
   View,
   Text,
@@ -14,27 +9,18 @@ import {
   StyleSheet,
   StatusBar,
   Alert,
-<<<<<<< HEAD
   Image,
 } from "react-native";
 
 import * as firebase from "firebase";
 
-=======
-  Image
-} from 'react-native';
- 
-import * as firebase from 'firebase';
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
 // import * as Animatable from 'react-native-animatable';
 // import LinearGradient from 'react-native-linear-gradient';
 // import FontAwesome from 'react-native-vector-icons/FontAwesome';
- 
+
 import { Ionicons } from "@expo/vector-icons";
 
 const LogIn = ({ navigation }) => {
-<<<<<<< HEAD
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -66,62 +52,20 @@ const LogIn = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Image source={require("../assets/logo4.png")} style={styles.img} />
-=======
- 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
- 
-  const onSignupPress = () => {
-    navigation.navigate('Signup')  }
- 
-  const onLoginPress = () => {
-    // check if empty
-    if(email == '' || password == ''){
-    alert('Please fill all requierd information')}
- 
-    firebase.auth().signInWithEmailAndPassword(email,password)
-    .then(() => {
-      setEmail('')
-      setPassword('')
-      navigation.navigate('Home')
-    }).catch((error) =>{
-      alert(error)
-    })
-  }
- 
-  const onLoading = () =>{
-    
-  }
- 
-  return (
-    <View style={styles.container}>
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
       <Text style={styles.welcome}>WELCOME</Text>
- 
+
       {/* E-mail */}
-<<<<<<< HEAD
       <View style={styles.inputFiled}>
         <Ionicons name="ios-mail" size={25} color="#646161"></Ionicons>
 
-=======
-      <View style={styles.inputFiled} >
-        <Ionicons name="ios-mail" size={25} color='#646161'></Ionicons>
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
         <TextInput
           placeholder={" Email"}
           onChangeText={(text) => setEmail(text)}
           value={email}
-<<<<<<< HEAD
           style={styles.textInputFiled}
         ></TextInput>
       </View>
 
-=======
-          style={styles.textInputFiled}></TextInput></View>
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
       {/* Passwoard */}
       <View style={styles.inputFiled}>
         <Ionicons name="ios-key" size={25} color="#646161"></Ionicons>
@@ -130,7 +74,6 @@ const LogIn = ({ navigation }) => {
           onChangeText={(text) => setPassword(text)}
           value={password}
           dsecureTextEntry
-<<<<<<< HEAD
           style={styles.textInputFiled}
         ></TextInput>
       </View>
@@ -155,39 +98,8 @@ const LogIn = ({ navigation }) => {
   );
 };
 
-=======
-          style={styles.textInputFiled}></TextInput>
- 
-        </View>
- 
-
-    {/* Login button */}
-      <TouchableOpacity style={styles.loginButton} 
-      underlayColor="#fff"
-      onPress={() => onLoginPress()} >
-        <Text
-          style={styles.loginText}>
-          Log in
-          </Text>
-      </TouchableOpacity>
- 
-    {/* Sign up button */}
-      <Text style={styles.text}>Dont have an account?
-            <Text style={styles.text2}
-          onPress={() => onSignupPress()}>
-          SignUp</Text>
-      </Text>
- 
-    </View>
-
-
-
-  )
-}
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
 export default LogIn;
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -202,6 +114,10 @@ const styles = StyleSheet.create({
     //marginTop: 10,
     color: "green",
     left: 70,
+  },
+  img: {
+    height: 200,
+    width: 200,
   },
   LogInButton: {
     color: "green",
@@ -222,10 +138,6 @@ const styles = StyleSheet.create({
   LogInButtonText: {
     color: "black",
     fontSize: 15,
-<<<<<<< HEAD
-=======
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
   },
   text: {
     color: "black",
@@ -234,12 +146,7 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
   text2: {
-<<<<<<< HEAD
     color: "green",
-=======
-    color: 'green',
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
   },
   inputFiled: {
     margin: 15,
@@ -261,7 +168,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.3,
     shadowRadius: 4.65,
- 
+
     elevation: 8,
   },
   textInputFiled: {
@@ -272,13 +179,8 @@ const styles = StyleSheet.create({
     height: 40,
     marginLeft: 65,
     marginTop: 20,
-<<<<<<< HEAD
-    bottom: 200,
-
-=======
     bottom: 240,
- 
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
+
     backgroundColor: "#EFF6F9",
     borderRadius: 10,
     borderWidth: 1,
@@ -291,23 +193,13 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.3,
     shadowRadius: 4.65,
- 
+
     elevation: 2,
   },
- 
+
   loginText: {
     color: "#060707",
     textAlign: "center",
     fontSize: 20,
   },
-<<<<<<< HEAD
-  img: {
-    width: 300,
-    height: 300,
-    marginLeft: 65,
-  },
 });
-=======
- 
-});
->>>>>>> f4ffffff0d855df6af9451ccddad80d4b9a28ee5
