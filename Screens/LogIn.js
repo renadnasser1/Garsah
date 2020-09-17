7547203ece21ef7d9d4b77037584c5c1322943b9
Amal Aldoss <amalabdullah13@gmail.com>
2020-09-17 10:42:58 +0300

    log in logo

--- Screens/LogIn.js
+++ Screens/LogIn.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 import {
   View,
@@ -9,10 +9,10 @@ import {
   StyleSheet,
   StatusBar,
   Alert,
-  Image
-} from 'react-native';
+  Image,
+} from "react-native";
 
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 // import * as Animatable from 'react-native-animatable';
 // import LinearGradient from 'react-native-linear-gradient';
@@ -20,167 +20,162 @@ import * as firebase from 'firebase';
 
 import { Ionicons } from "@expo/vector-icons";
 
-
-
 const LogIn = ({ navigation }) => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSignupPress = () => {
-    navigation.navigate('Signup')  }
+    navigation.navigate("Signup");
+  };
 
   const onLoginPress = () => {
     // check if empty
-    if(email == '' || password == ''){
-    alert('Please fill all requierd information')}
+    if (email == "" || password == "") {
+      alert("Please fill all requierd information");
+    }
 
-    firebase.auth().signInWithEmailAndPassword(email,password)
-    .then(() => {
-      setEmail('')
-      setPassword('')
-      navigation.navigate('Home')
-    }).catch((error) =>{
-      alert(error)
-    })
-  }
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        setEmail("");
+        setPassword("");
+        navigation.navigate("Home");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
 
-  const onLoading = () =>{
-    
-  }
+  const onLoading = () => {};
 
   return (
     <View style={styles.container}>
-
+      <Image source={require("../assets/logo4.png")} style={styles.img} />
       <Text style={styles.welcome}>WELCOME</Text>
 
       {/* E-mail */}
-      <View style={styles.inputFiled} >
-        <Ionicons name="ios-mail" size={25} color='#646161'></Ionicons>
+      <View style={styles.inputFiled}>
+        <Ionicons name="ios-mail" size={25} color="#646161"></Ionicons>
 
         <TextInput
-          placeholder={' Email'} 
+          placeholder={" Email"}
           onChangeText={(text) => setEmail(text)}
           value={email}
-          style={styles.textInputFiled}></TextInput></View>
+          style={styles.textInputFiled}
+        ></TextInput>
+      </View>
 
       {/* Passwoard */}
-      <View style={styles.inputFiled} >
-        <Ionicons name="ios-key" size={25} color='#646161'></Ionicons>
+      <View style={styles.inputFiled}>
+        <Ionicons name="ios-key" size={25} color="#646161"></Ionicons>
         <TextInput
-          placeholder={' Password'} 
+          placeholder={" Password"}
           onChangeText={(text) => setPassword(text)}
           value={password}
           dsecureTextEntry
-          style={styles.textInputFiled}></TextInput>
+          style={styles.textInputFiled}
+        ></TextInput>
+      </View>
 
-        </View>
-
-
-    {/* Login button */}
-      <TouchableOpacity style={styles.loginButton} 
-      underlayColor="#fff"
-      onPress={() => onLoginPress()} >
-        <Text
-          style={styles.loginText}>
-          Log in
-          </Text>
+      {/* Login button */}
+      <TouchableOpacity
+        style={styles.loginButton}
+        underlayColor="#fff"
+        onPress={() => onLoginPress()}
+      >
+        <Text style={styles.loginText}>Log in</Text>
       </TouchableOpacity>
 
-    {/* Sign up button */}
-      <Text style={styles.text}>Dont have an account?
-            <Text style={styles.text2}
-          onPress={() => onSignupPress()}>
-          SignUp</Text>
+      {/* Sign up button */}
+      <Text style={styles.text}>
+        Dont have an account?
+        <Text style={styles.text2} onPress={() => onSignupPress()}>
+          SignUp
+        </Text>
       </Text>
-
     </View>
-
-
-
-  )
-}
+  );
+};
 
 export default LogIn;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'flex-end',
+    backgroundColor: "white",
+    justifyContent: "flex-end",
   },
   welcome: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     paddingHorizontal: 20,
     fontSize: 50,
-    marginTop: 210,
+    //marginTop: 10,
     color: "green",
-    left: 70
+    left: 70,
   },
   LogInButton: {
-    color: 'green',
+    color: "green",
     margin: 20,
     height: 40,
     width: 200,
-    borderColor: 'grey',
+    borderColor: "grey",
     borderWidth: 1,
     borderRadius: 30,
     bottom: 220,
     paddingHorizontal: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     left: 90,
-    backgroundColor: '#EFF6F9',
-    borderColor: '#EFF6F9'
+    backgroundColor: "#EFF6F9",
+    borderColor: "#EFF6F9",
   },
   LogInButtonText: {
-    color: 'black',
+    color: "black",
     fontSize: 15,
-
   },
   text: {
-    color: 'black',
+    color: "black",
     left: 100,
-    bottom: 220,
+    bottom: 200,
     marginLeft: 10,
   },
   text2: {
-    color: 'green',
-
+    color: "green",
   },
   inputFiled: {
     margin: 15,
     padding: 8,
     width: 280,
     height: 40,
-    bottom: 300,
+    bottom: 200,
     left: 50,
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
     borderBottomEndRadius: 10,
     borderTopRightRadius: 10,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    backgroundColor: "#fff",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 4,
     },
-    shadowOpacity: 0.30,
+    shadowOpacity: 0.3,
     shadowRadius: 4.65,
 
     elevation: 8,
   },
   textInputFiled: {
-    width: 200
+    width: 200,
   },
   loginButton: {
     width: 280,
     height: 40,
     marginLeft: 65,
     marginTop: 20,
-    bottom: 240,
+    bottom: 200,
 
     backgroundColor: "#EFF6F9",
     borderRadius: 10,
@@ -203,5 +198,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 20,
   },
-
+  img: {
+    width: 300,
+    height: 300,
+    marginLeft: 65,
+  },
 });
