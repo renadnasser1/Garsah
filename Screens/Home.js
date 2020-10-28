8ef84c0568219b74439613ebbc60d057e3d8e4b0
Nouf AlSubaie <63476024+Noufalsubaie@users.noreply.github.com>
2020-10-28 20:39:23 +0300

    changes 12

--- Screens/Home.js
+++ Screens/Home.js
@@ -278,7 +278,7 @@ export default class Home extends React.Component {
 
    </View>
 
-   <Text style={[styles.text,{alignSelf:'center',marginVertical:'40%'}]} >Welcome to homepage branch 🌱</Text>
+   <Text style={[styles.text,{alignSelf:'center',marginVertical:'40%'}]} >Welcome to homepage branch #22🌱</Text>
 
              
  </View>
